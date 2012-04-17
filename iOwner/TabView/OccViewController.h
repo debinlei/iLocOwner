1418fcfc70412d9d047bc8d348ebfeef10f0f09d
debinlei <debin.lei@gmail.com>
2012-04-17 09:29:01 +0800

    add the view controller for the occupy action

---
+++ iOwner/TabView/OccViewController.h
@@ -0,0 +1,13 @@
+//
+//  OccViewController.h
+//  iOwner
+//
+//  Created by ldb on 4/17/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OccViewController : UIViewController
+
+@end
---
+++ iOwner/TabView/OccViewController.m
@@ -0,0 +1,44 @@
+//
+//  OccViewController.m
+//  iOwner
+//
+//  Created by ldb on 4/17/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "OccViewController.h"
+
+@interface OccViewController ()
+
+@end
+
+@implementation OccViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
